cb91560bf065d5668cef21b916003c513e026604
mrock724 <mrock724@gmail.com>
2024-08-10 18:50:45 -0400

    Revised scene change script

--- boundaries/Scripts/nextscenechanger.gd
+++ boundaries/Scripts/nextscenechanger.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-@export var nextscenes = ["res://Scenes/level_2.tscn", "res://Scenes/level_3.tscn"]
+@export var nextscenes = ["res://Scenes/level_2.tscn", "res://Scenes/level_3.tscn", "res://Scenes/level_4.tscn"]
 func _process(delta):
-	if $"..".name == "level1":
-		$"../winnode".file = nextscenes[0]
-	if $"..".name == "level2":
-		$"../winnode".file = nextscenes[1]
-	
+	match $"..".name:
+		"level1":
+			$"../winnode".file = nextscenes[0]
+		"level2":
+			$"../winnode".file = nextscenes[1]
+		"level3":
+			$"../winnode".file = nextscenes[2]
+		_:
+			pass
