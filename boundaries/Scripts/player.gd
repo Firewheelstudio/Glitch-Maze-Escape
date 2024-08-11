ca567f6b196175a282b0776a8998301da9c07cc9
jaedablea <jaedablea@gmail.com>
2024-08-10 19:06:57 -0500

    fixed animations
    sorted animations, added scripts for animations and fixed resolution issues.
    Organized animation files

--- boundaries/Scripts/player.gd
+++ boundaries/Scripts/player.gd
@@ -2,12 +2,29 @@ extends CharacterBody2D
 var speed = 800
 var accel = 1500
 var canmove = false
+
 func _process(delta):
 	var x_axis = Input.get_axis("move_left", "move_right")
 	var y_axis = Input.get_axis("move_up", "move_down")
 	var input := Vector2(x_axis, y_axis)
 	if canmove == true:
 		position += input * speed * delta
+		
+	if x_axis > 0 && canmove:
+		$AnimatedSprite2D.play("side_walk")
+		$AnimatedSprite2D.flip_h = false;
+	if x_axis < 0 && canmove:
+		$AnimatedSprite2D.play("side_walk")
+		$AnimatedSprite2D.flip_h = true;
+	if x_axis == 0 && $AnimatedSprite2D.animation == "side_walk":
+		$AnimatedSprite2D.play("side_idle")
+		
+	if y_axis > 0 && x_axis == 0 && canmove:
+		$AnimatedSprite2D.play("front_walk")
+		
+	if y_axis == 0 && $AnimatedSprite2D.animation == "front_walk":
+		$AnimatedSprite2D.play("front_idle")
+	
 #	if input == Vector2.ZERO:
 #		velocity = Vector2.ZERO
 #	else:
