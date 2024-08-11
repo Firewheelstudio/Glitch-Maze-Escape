8fd336a5223e132c31fd59d3a0f59a926f2d4e36
Davedalus <89887017+Davedalus@users.noreply.github.com>
2024-08-11 01:24:16 -0600

    fixed enemy behavior
    enemy moves after button is pressed

--- boundaries/Scripts/CanvasLayer.gd
+++ boundaries/Scripts/CanvasLayer.gd
@@ -6,3 +6,4 @@ func _on_button_button_down():
 	$Button.hide()
 	$Label2.hide()
 	$"../..".canmove = true
+	
--- boundaries/enemy_node.gd
+++ boundaries/enemy_node.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var speed = 700
+var canmove = false
 
 signal captured_player
 
@@ -12,11 +13,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var vector_to_player = position - $"../playernode".position
-	vector_to_player = vector_to_player.normalized()
-	var velocity = vector_to_player * speed
-	position -= velocity * delta
+	if canmove:
+		var vector_to_player = position - $"../playernode".position
+		vector_to_player = vector_to_player.normalized()
+		var velocity = vector_to_player * speed
+		position -= velocity * delta
 
 func _on_area_2d_body_entered(body):
 	if body.name == "playernode": 
 		captured_player.emit()# Replace with function body.
+
+
+func _on_button_button_down():
+	canmove = true # Replace with function body.
