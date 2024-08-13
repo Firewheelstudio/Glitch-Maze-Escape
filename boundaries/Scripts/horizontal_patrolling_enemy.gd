9ced6bc2e01681a6123a0a5d49cae444b8225a92
Va1epp <bpmpochta@gmail.com>
2024-08-13 12:39:27 +0500

    Horizontal enemy's animations fixed

--- boundaries/Scripts/horizontal_patrolling_enemy.gd
+++ boundaries/Scripts/horizontal_patrolling_enemy.gd
@@ -14,17 +14,17 @@ func _process(delta):
 	var velocity = speed * vector
 	position += velocity * delta
 	handle_animation()
+	$CharacterBody2D/Area2D/AnimatedSprite2D.animation == "backward"
+	$CharacterBody2D/Area2D/AnimatedSprite2D.play("backward")
 
 func handle_animation():
 	match vector:
 		Vector2.LEFT:
-			$CharacterBody2D/Area2D/AnimatedSprite2D.animation == "forward"
-			$CharacterBody2D/Area2D/AnimatedSprite2D.play("forward")
+			$CharacterBody2D/Area2D/AnimatedSprite2D.flip_h = true
+		
 		Vector2.RIGHT:
-			$CharacterBody2D/Area2D/AnimatedSprite2D.animation == "backward"
-			$CharacterBody2D/Area2D/AnimatedSprite2D.play("backward")
-
-
+			$CharacterBody2D/Area2D/AnimatedSprite2D.flip_h = false
+		
 func _on_area_2d_body_entered(body):
 	match body.name:
 		"TileMap":
